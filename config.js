aaa4fbaff870e67d6ee3adbe2c0e7f843f0f7691
mzunruly8 <mzunruly8@gmail.com>
2026-01-27 21:48:57 -0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+    canvasWidth: 420,
+    canvasHeight: 420,
+    gridSize: 6,
+    tileSize: 60,
+    lineWidth: 4,
+    colors: {
+        glow: "#2de2e6",
+        bg: "#000"
+    }
+};
